a83f75077b9fa98556cd8d9e92c01bce18234869
Restu Astikadeni <gderestuastika04@gmail.com>
2026-04-15 19:17:30 +0800

    start

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Selamat Datang di BAPAS Website Inovasi</h1>
+          <p>Temukan berbagai inovasi terbaru dan informasi menarik seputar BAPAS di sini.</p>
+          <h1>COMING SOON</h1>
+    </div>
+  );
+}
